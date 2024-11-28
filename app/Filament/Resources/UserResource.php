a189dd17229de04d3b8f512219489a57f66eaab6
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-28 19:16:11 +0700

    user resources

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // 
+                //card
+                Forms\Components\Card::make()
+                    ->schema([
+                        //name
+                        Forms\Components\TextInput::make('name')
+                            ->label('Nama')
+                            ->placeholder('Nama')
+                            ->required(),
+                        Forms\Components\TextInput::make('email')
+                            ->label('Email')
+                            ->placeholder('Email')
+                            ->required()
+                            ->email(),
+                        //password
+                        Forms\Components\TextInput::make('password')
+                            ->label('Password')
+                            ->placeholder('Password')
+                            ->required()
+                            ->password(),
+                        //password_confirmation
+                        Forms\Components\TextInput::make('password_confirmation')
+                            ->label('Konfirmasi Password')
+                            ->placeholder('Konfirmasi Password')
+                            ->required()
+                            ->password(),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('email')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
